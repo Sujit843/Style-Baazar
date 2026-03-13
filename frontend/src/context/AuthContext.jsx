103b3c758917a5db737cba66fe76631829fef2c2
Sujit843 <sujitkrs92037@gmail.com>
2026-03-13 14:33:34 +0530

    Fix context import capitalization

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,17 +1,16 @@
 import { createContext } from "react";
 export const authDataContext = createContext();
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:5000";
+    let serverUrl = "https://style-baazar.onrender.com";
     let value = {
         serverUrl
     }
   return (
 
-    <div>
         <authDataContext.Provider value={value}>
             {children}
         </authDataContext.Provider>
-    </div>
+    
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-import AuthContext from './context/authContext.jsx';
 import UserContext from './context/UserContext.jsx';
 import ShopContext from './context/ShopContext.jsx';
 import ScrollToTop from './components/ScrollToTop.jsx';
 import WishlistProvider from './context/WishlistContext.jsx';
+import AuthContext from './context/authContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
